4b222ee4c4873e7c1297a8c9be4bbca0da20e774
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-12-27 23:25:51 +0530

    added register user controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
     {
-        username: {
+        userName: {
             type: String,
             required: true,
             unique: true,
@@ -54,7 +54,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function(next){
     if(this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -67,7 +67,7 @@ userSchema.methods.generateRefreshToken = function(){
         {
             _id: this._id,
             email: this.email,
-            username: this.username,
+            userName: this.userName,
             fullName: this.fullName,
         },
         process.env.REFRESH_TOKEN_SECRET,
@@ -82,7 +82,7 @@ userSchema.methods.generateAccessToken = function(){
         {
             _id: this._id,
             email: this.email,
-            username: this.username,
+            userName: this.userName,
             fullName: this.fullName,
         },
         process.env.ACCESS_TOKEN_SECRET,
