bdde9c524707dbce68350c8fb31004ec104f6031
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-12-28 23:58:09 +0530

    bug fixes user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,11 +7,11 @@ const router = Router()
 router.route("/register").post(
     upload.fields([
         {
-            name: "profile",
+            name: "avatar",
             maxCount: 1
         },
         {
-            name: "cover",
+            name: "coverImage",
             maxCount: 1
         }
     ]),
